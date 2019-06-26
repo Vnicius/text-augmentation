643ac0c2da30d9eb2cfbce93b4a62e56635ebef9
vnicius <vinicius.matheus@gmail.com>
2019-06-26 10:07:13 -0300

    Add generate data on train

--- preprocess.py
+++ preprocess.py
@@ -7,17 +7,13 @@ from keras.utils import to_categorical
 import numpy as np
 from src.WordsDictionary import WordsDictionary
 from src.args.PreprocessArgs import PreprocessArgs
+from src.utils.utils import crate_dir
 
 POINTS = re.compile(r'([\.\,\?\!])(\D|$)')
 SYMBOLS = re.compile(r'([\(\)\[\]\{\}])')
 QUOTES = re.compile(r'\'([^\']+)\'|$')
 DB_QUOTES = re.compile(r'\"([^\"]+)\"|$')
 TREE_DOTS = re.compile(r'\.(\s+\.)+')
-
-
-def crate_dir(dir_name):
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
 
 
 def preprocess(text):
@@ -41,14 +37,9 @@ def preprocess(text):
 
 def generate_src_tgt(text):
     text_words = text.split(' ')
-    src = []
-    tgt = []
 
     for i in range(1, len(text_words)):
-        src.append(' '.join(text_words[:i]))
-        tgt.append(text_words[i])
-
-    return src, tgt
+        yield((' '.join(text_words[:i]), text_words[i]))
 
 
 def main(input_file, output_dir, max_size=0):
@@ -57,14 +48,14 @@ def main(input_file, output_dir, max_size=0):
     infos = {}
     max_len = 0
 
-    with open(input_file, 'r', encoding='utf-8') as input_text, open(os.path.join(output_dir, 'data.src'), 'w', encoding='utf-8') as output_src, open(os.path.join(output_dir, 'data.tgt'), 'w', encoding='utf-8') as output_tgt:
+    with open(input_file, 'r', encoding='utf-8') as input_text, open(os.path.join(output_dir, 'data.train'), 'w', encoding='utf-8') as output_train:
         try:
             while True:
                 text = input_text.readline().replace("\n", '')
 
                 if not len(text):
                     break
-            
+
                 text = preprocess(text)
                 text_size = len(text.split(' '))
 
@@ -75,22 +66,25 @@ def main(input_file, output_dir, max_size=0):
                     if text_size > max_len:
                         max_len = text_size
 
-                words_dict.add_text(text)               
-                src, tgt = generate_src_tgt(text)
+                words_dict.add_text(text)
+                output_train.write(text + "\n")
+                # src, tgt = generate_src_tgt(text)
 
-                for s, t in zip(src, tgt):
-                    output_src.write(s + '\n')
-                    output_tgt.write(t + '\n')
+                # for s, t in zip(src, tgt):
+                #     output_src.write(s + '\n')
+                #     output_tgt.write(t + '\n')
         except EOFError:
             pass
 
-    words_dict.build_dict()
-    pickle.dump(words_dict, open(os.path.join(output_dir, 'dact.dict'), 'wb'))
+    words_dict.build_dict(32000)
+    pickle.dump(words_dict, open(os.path.join(output_dir, 'data.dict'), 'wb'))
 
     with open(os.path.join(output_dir, 'data.meta.json'), 'w', encoding='utf-8') as infos_file:
         infos['max_seq_size'] = max_size if max_size else max_len
+        infos['dict_size'] = len(words_dict.word_to_int)
 
         json.dump(infos, infos_file)
+
 
 def text_to_vec(text, word_dict, max_size=0):
     text_vector = word_dict.text_to_int(text)
@@ -104,6 +98,7 @@ def text_to_vec(text, word_dict, max_size=0):
 
     return vector
 
+
 def vec_to_text(vector, word_dict):
     return word_dict.int_to_text(vector)
 
@@ -113,6 +108,7 @@ def word_to_vec(word, word_dict):
     word_vec = to_categorical(
         word_vec, num_classes=len(word_dict.word_to_int))
     return word_vec
+
 
 def word_oh_to_int(word_oh):
     return np.argmax(word_oh)
--- train.py
+++ train.py
@@ -2,10 +2,14 @@
 from os import path
 import pickle
 import json
+from random import shuffle
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Activation, Embedding, Bidirectional 
-from preprocess import text_to_vec, preprocess
+from keras.layers import Dense, LSTM, Activation, Embedding, Bidirectional
+from preprocess import text_to_vec, preprocess, word_to_vec, generate_src_tgt
+from src.utils.utils import crate_dir
+from src.args.TrainArgs import TrainArgs
+
 
 def create_model(dict_len, max_seq_len, embedding_size=32, lstm_size=128):
     model = Sequential()
@@ -13,29 +17,54 @@ def create_model(dict_len, max_seq_len, embedding_size=32, lstm_size=128):
     model.add(Bidirectional(LSTM(lstm_size)))
     model.add(Dense(dict_len))
     model.add(Activation('softmax'))
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="adam", metrics=['accuracy'])
     return model
 
-def train(model, x_data, y_data, batch_size, epochs):
-    return model.fit(x=x_data, y=y_data, epochs=epochs, batch_size=batch_size, verbose=1, validation_split=0.2, shuffle=True)
 
-def main(data_dir, output_dir, embedding_size, lstm_size, batch_size, epochs):
-    with open(path.join(data_dir, 'data.src'), 'r', encoding='utf-8') as src_data, open(path.join(data_dir, 'data.tgt'), 'r', encoding='utf-8') as tgt_data, open(path.join(data_dir, 'data.dict'), 'rb') as data_dict, open(path.join(data_dir, 'data.meta.json')) as meta_file:
+# def train(model, x_data, y_data, batch_size, epochs):
+#     return model.fit(x=x_data, y=y_data, epochs=epochs, batch_size=batch_size, verbose=1, validation_split=0.2, shuffle=True)
+
+
+def train_generator(model, generator, steps_per_epoch, epochs):
+    return model.fit_generator(generator, steps_per_epoch=steps_per_epoch, epochs=epochs)
+
+
+def generate_arrays_from_file(path_train_data, word_dict, meta):
+    while True:
+        with open(path_train_data) as data_train:
+            for line in data_train:
+                data = list(generate_src_tgt(line.replace('\n', '')))
+                shuffle(data)
+
+                for d in data:
+                    x = text_to_vec(preprocess(d[0]), word_dict, meta['max_seq_size'])
+                    y = word_to_vec(preprocess(d[1]), word_dict)
+
+                    yield (np.array([x]), np.array(y))
+
+
+def main(data_dir, output_dir, embedding_size, lstm_size, steps_per_epoch, epochs):
+    crate_dir(output_dir)
+
+    with open(path.join(data_dir, 'data.dict'), 'rb') as data_dict, open(path.join(data_dir, 'data.meta.json')) as meta_file:
         meta = json.load(meta_file)
         word_dict = pickle.load(data_dict)
-        x_data = []
+        model = create_model(meta['dict_size'], meta['max_seq_size'],
+                             embedding_size=embedding_size, lstm_size=lstm_size)
 
-        try:
-            while True:
-                x_data.append(text_to_vec(preprocess(src_data.readline()), word_dict,meta['max_seq_size']))
-        except EOFError:
-            pass
-        
-        x_data = np.array(x_data)
+        train_generator(model, generate_arrays_from_file(path.join(data_dir, 'data.train'), word_dict, meta), steps_per_epoch, epochs)
 
-        print(x_data.shape)
+        json_model = model.to_json()
 
+        with open(path.join(output_dir, 'model.json'), "w") as json_file:
+            json_file.write(json_model)
+
+        model.save_weights(path.join(output_dir, 'model.h5'))
 
 
 if __name__ == '__main__':
-    main('prep', 'out', 32, 128, 2, 2)
+    args = TrainArgs().args
+
+    main(args.data_dir, args.output, embedding_size=args.embedding_size,
+         lstm_size=args.lstm_size, steps_per_epoch=args.steps, epochs=args.epochs)
